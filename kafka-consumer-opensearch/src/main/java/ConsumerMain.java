b4dd63b302e979e04131c344bae5eec1829d8c35
Keshav Ranjan <ranjan.keshav@gmail.com>
2022-07-24 12:39:34 -0700

    Added docker compose for opensearch and dashboard.

--- kafka-consumer-opensearch/src/main/java/ConsumerMain.java
+++
@@ -1,6 +0,0 @@
-public class ConsumerMain {
-
-  public static void main(String[] args) {
-    System.out.println("Main Class");
-  }
-}
---
+++ kafka-consumer-opensearch/src/main/java/OpenSearchConsumerMain.java
@@ -0,0 +1,15 @@
+public class OpenSearchConsumerMain {
+
+    public static void main(String[] args) {
+
+
+        // Create an  OpenSearch client
+        //Create a kafka Client
+
+        //main logic code
+        // Close things
+
+
+        System.out.println("Main Class");
+    }
+}
